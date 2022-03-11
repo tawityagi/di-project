cdfab7e1aa05ee46e229ea284db0b3609238d643
tawityagi <tawityagi@gmail.com>
2022-03-11 10:48:03 +0530

    Initialised Spring Project

---
+++ src/main/java/tyagi/tawi/diproject/DiProjectApplication.java
@@ -0,0 +1,13 @@
+package tyagi.tawi.diproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiProjectApplication.class, args);
+	}
+
+}
