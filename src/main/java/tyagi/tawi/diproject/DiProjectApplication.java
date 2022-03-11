2ab2add4dbe11f54a1971325ee52083984efb755
tawityagi <tawityagi@gmail.com>
2022-03-11 11:20:53 +0530

    Example 1 : Spring Context

--- src/main/java/tyagi/tawi/diproject/DiProjectApplication.java
+++ src/main/java/tyagi/tawi/diproject/DiProjectApplication.java
@@ -2,12 +2,20 @@ package tyagi.tawi.diproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import tyagi.tawi.diproject.controllers.MyController;
 
 @SpringBootApplication
 public class DiProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiProjectApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DiProjectApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
